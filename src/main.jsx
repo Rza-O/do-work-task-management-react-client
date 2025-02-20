f710a8efcb7afa82fcc6dd86f36c1236c8ef5611
appoclayptor <shahreza1414@gmail.com>
2025-02-21 01:46:33 +0600

    Implement routing with React Router and replace App component in main entry point

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,13 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import AuthProvider from './Providers/AuthProvider.jsx'
+import { RouterProvider } from 'react-router-dom'
+import { routes } from './Routes/routes.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <AuthProvider>
-      <App />
+      <RouterProvider router={routes} /> 
     </AuthProvider>
   </StrictMode>,
 )
