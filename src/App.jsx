8a47d3d8189cf952b9afd6ac80d7fb325ee26dda
appoclayptor <shahreza1414@gmail.com>
2025-02-20 23:21:04 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <h2 className='text-5xl'>Do Work</h2>
+      <p>Smart solution for task management!</p>
+    </>
+  )
+}
+
+export default App
