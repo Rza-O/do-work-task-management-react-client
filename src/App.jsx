42b419f704e5b2ed8a458b9dbc7057e23788a7c0
appoclayptor <shahreza1414@gmail.com>
2025-02-21 01:34:51 +0600

    Refactor Google login handling to use custom hook and improve button functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,20 @@
 import './App.css'
+import useAuth from './Hooks/useAuth'
 
 function App() {
+  const { handleGoogleLogin } = useAuth();
 
-  const handleGoogleLogin = () => {
-    
+  const handleSocialLogin = () => {
+    handleGoogleLogin()
+      .then(res => console.log('login successful!'))
+      .catch(err => console.log(err))
   }
 
   return (
     <>
       <h2 className='text-5xl'>Do Work</h2>
       <p>Smart solution for task management!</p>
-      <button>Click me to google login</button>
+      <button onClick={handleSocialLogin}>Click me to google login</button>
     </>
   )
 }
