870a16d468b001e49741352e990c9ae1558276e8
appoclayptor <shahreza1414@gmail.com>
2025-02-21 07:31:14 +0600

    Refactor taskServices to use API_URL constant, and implement CRUD operations for tasks

--- src/lib/taskServices.js
+++ src/lib/taskServices.js
@@ -1,8 +1,28 @@
 import axios from "axios";
 
-const API = import.meta.env.VITE_server_link;
+const API_URL = import.meta.env.VITE_server_link;
 
+// getting all tasks
 export const fetchTasks = async () => {
-   const { data } = await axios(API);
+   const { data } = await axios(API_URL);
    return data;
 }
+
+// adding a new task
+export const addTask = async (task) => {
+   const { data } = await axios.post(API_URL, task);
+   return data;
+}
+
+// updating a new task
+export const updateTask = async (id, updatedTask) => {
+   const { data } = await axios.put(`${API_URL}/${id}`, updatedTask);
+   return data
+}
+
+// deleting a task
+export const deleteTask = async (id) => {
+   const { data } = await axios.delete(`${API_URL}/${id}`);
+   return data;
+}
+
